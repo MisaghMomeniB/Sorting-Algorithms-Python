14c4ac864ddf4e3e87ec22fe5ae21b236ba4f0dd
MisaghMomeniB <jenabemisagh@gmail.com>
2025-03-14 19:02:35 +0330

    main

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from bubble_sort import bubble_sort
+from selection_sort import selection_sort
+from insertion_sort import insertion_sort
+from quick_sort import quick_sort
+from merge_sort import merge_sort
+
+arr = [64, 25, 12, 22, 11]
+print("Bubble Sort:", bubble_sort(arr[:]))
+print("Selection Sort:", selection_sort(arr[:]))
+print("Insertion Sort:", insertion_sort(arr[:]))
+print("Quick Sort:", quick_sort(arr[:]))
+print("Merge Sort:", merge_sort(arr[:]))
